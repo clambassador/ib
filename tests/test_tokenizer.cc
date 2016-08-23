6dfe4c29dcaa4eb9b5031163dfdfad6a30b8537b
Joel Reardon <joel.reardon@gmail.com>
2016-08-23 00:42:24 -0700

    Added a tokenizer reader to extract strings/values from strings with a format

--- tests/test_tokenizer.cc
+++ tests/test_tokenizer.cc
@@ -16,4 +16,28 @@ int main() {
 	Tokenizer::split_mind_quote("hello there \"good people\"", " ", &tokens);
 	Logger::info("%", tokens);
 	tokens.clear();
+
+	int val = 0;
+	int ret = Tokenizer::extract("(%)", "(42)", &val);
+	assert(val == 42);
+	assert(ret == 1);
+	string str1, str2;
+	ret = Tokenizer::extract("(%)%(%)", "(2)hello(toot)", &val, &str1, &str2);
+	assert(ret == 3);
+	assert(val == 2);
+	assert(str1 == "hello");
+	assert(str2 == "toot");
+
+	ret = Tokenizer::extract("%--? %", "preamble--? 18", &str1, &val);
+	assert(val == 18);
+	assert(str1 == "preamble");
+	assert(ret == 2);
+
+	/*
+TODO: compare format string to data string with a doubling of percents.
+TODO: allow other symbols than percent to allow reading 5 from 5%
+	ret = Tokenizer::extract("% %%", "5 %", &str1);
+	assert(str1 == "5");
+	assert(ret == 1);
+	*/
 }
--- tokenizer.h
+++ tokenizer.h
@@ -57,6 +57,21 @@ public:
 		}
 	}
 
+	static void split_with_empty(const string& data, const string& deliminator,
+		                     vector<string>* tokens) {
+		size_t pos = 0;
+		while (true) {
+			size_t start = pos;
+			pos = data.find(deliminator, start);
+			if (pos == string::npos) {
+				tokens->push_back(data.substr(start));
+				break;
+			}
+			tokens->push_back(data.substr(start, pos - start));
+			pos += deliminator.length();
+		}
+	}
+
 	static void split_mind_quote(const string& data, const string& deliminator,
 				     vector<string>* tokens) {
 		size_t pos = 0;
@@ -75,6 +90,90 @@ public:
 			}
 			pos += deliminator.length();
 		}
+	}
+
+	template<typename... ARGS>
+	static size_t extract(const string& format, const string& data,
+			      ARGS... args) {
+		size_t pos_format = 0;
+		size_t pos_data = 0;
+
+		if (data.empty()) return 0;
+		if (format.empty()) return 0;
+
+		try {
+		vector<string> tokens_with_empty;
+		list<string> tokens;
+		split_with_empty(format, "%", &tokens_with_empty);
+		for (size_t i = 0; i < tokens_with_empty.size(); ++i) {
+			if (tokens_with_empty[i].empty()) {
+				if (tokens.empty()) {
+					assert(!i);
+					tokens.push_back("");
+					if (format.length() > 1 &&
+					    format[1] != '%')
+						continue; // initial
+				}
+				if (i + 1 == tokens_with_empty.size()) {
+					tokens.push_back("");
+					continue;
+				}
+				string token = tokens.back();
+				token += "%" + tokens_with_empty[++i];
+				tokens.pop_back();
+				tokens.push_back(token);
+
+			} else {
+				tokens.push_back(tokens_with_empty[i]);
+			}
+		}
+		return Tokenizer::extract_impl(
+			pos_format, format, pos_data, data,
+			&tokens, 0, args...);
+		} catch (int e) {
+			return 0;  // TODO: replace assert with throws
+		}
+	}
+
+	template<typename T, typename... ARGS>
+	static size_t extract_impl(size_t pos_format, const string& format,
+				   size_t pos_data, const string& data,
+				   list<string>* tokens, int cnt,
+				   T* car, ARGS... cdr) {
+		string token = tokens->front();
+		tokens->pop_front();
+		assert(tokens->size());
+		assert(format.substr(pos_format, token.length()) ==
+		       data.substr(pos_data, token.length()));
+		pos_format += token.length();
+		pos_data += token.length();
+
+		size_t nextpos = data.find(tokens->front(), pos_data);
+
+		if (nextpos == string::npos) return 0;
+		if (tokens->front().empty()) nextpos = data.length();
+
+		stringstream ss;
+		ss << data.substr(pos_data, nextpos - pos_data);
+		ss >> *car;
+
+		pos_data = nextpos;
+		pos_format += 1;
+
+		return Tokenizer::extract_impl(
+			pos_format, format, pos_data, data,
+			tokens, cnt + 1, cdr...);
+	}
+
+	// base case
+	static size_t extract_impl(size_t pos_format, const string& format,
+				   size_t pos_data, const string& data,
+				   list<string>* tokens, int cnt) {
+		assert(tokens->size() == 1);
+		if (tokens->empty()) return data.length();
+		assert(data.substr(pos_data) == tokens->front());
+		assert(format.substr(pos_format) == tokens->front());
+		return cnt;
 	}
 };
 
