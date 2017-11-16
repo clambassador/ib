531db296e72644087e451062445281aa1efb5b69
Joel Reardon <joel.reardon@gmail.com>
2017-11-16 13:12:38 -0700

    edits

--- logger.cc
+++ logger.cc
@@ -3,7 +3,6 @@
 namespace ib {
 
 unique_ptr<mutex> Logger::_mutex(new mutex());
-function<void()> Logger::_fn(bind(&exit,0));
 int Logger::_cur_fd = 0;
 unordered_map<int, FILE *> Logger::_fd_to_file;
 
