bfbb92a026e58c5fb1b3f688b46a69945aed51d3
Joel Reardon <joel.reardon@gmail.com>
2019-02-01 17:45:33 -0700

    added huge buffer for damaged gzip files

--- zcat.h
+++ zcat.h
@@ -13,17 +13,30 @@ namespace {
 
 class ZCat {
 public:
-	static string zcat(const string& data) {
-		return zcat(data, nullptr);
+	static string huge_zcat(const string& data) {
+		return huge_zcat(data, nullptr);
 	}
 
-	static string zcat(const string& data, size_t* leftover) {
-		const int CHUNK = 1 << 12;
+	static string huge_zcat(const string& data, size_t* leftover) {
+		static unique_ptr<uint8_t> out;
+		const size_t CHUNK = 1 << 25;
+		if (out.get() == nullptr) {
+			out.reset(new uint8_t[CHUNK]);
+		}
+
+		return zcat(data, nullptr, out.get(), CHUNK);
+	}
+
+	static string zcat(const string& data) {
+		uint8_t buf[1 << 17];
+		return zcat(data, nullptr, buf, 1 << 17);
+	}
+
+	static string zcat(const string& data, size_t* leftover,
+			   uint8_t *out, size_t CHUNK) {
 		int ret;
 		unsigned have;
 		z_stream strm;
-		unsigned char in[CHUNK];
-		unsigned char out[CHUNK];
 
 		strm.zalloc = Z_NULL;
 		strm.zfree = Z_NULL;
@@ -34,33 +47,26 @@ public:
 		ret = inflateInit2(&strm, 9 + 16);
 		assert(ret == Z_OK);
 
-		size_t pos = 0;
 		stringstream ss;
+		strm.avail_in = data.length();
+		strm.next_in = (uint8_t*) data.c_str();
 		do {
-			size_t N = CHUNK;
-			if (data.length() - pos < CHUNK)
-				N = data.length() - pos;
-			strm.avail_in = N;
-			memcpy(in, data.c_str() + pos, N);
-			pos += N;
-			strm.next_in = in;
-			do {
-				strm.avail_out = CHUNK;
-				strm.next_out = out;
-				ret = inflate(&strm, Z_NO_FLUSH);
-				assert(ret != Z_STREAM_ERROR);
-				assert(ret != Z_NEED_DICT);
-				have = CHUNK - strm.avail_out;
-				if (ret == Z_DATA_ERROR || ret == Z_MEM_ERROR) {
-					assert(!have);
-					inflateEnd(&strm);
-					return ss.str();
-				}
+			strm.avail_out = CHUNK;
+			strm.next_out = out;
+			ret = inflate(&strm, Z_FINISH);
+			assert(ret != Z_STREAM_ERROR);
+			assert(ret != Z_NEED_DICT);
+			have = CHUNK - strm.avail_out;
+			if (ret == Z_DATA_ERROR || ret == Z_MEM_ERROR) {
 				ss << string((char*) out, have);
-			} while (strm.avail_out == 0);
-		} while (ret != Z_STREAM_END);
-		inflateEnd(&strm);
+				if (leftover) *leftover = strm.avail_in;
+				inflateEnd(&strm);
+				return ss.str();
+			}
+			ss << string((char*) out, have);
+		} while (strm.avail_out == 0);
 		if (leftover) *leftover = strm.avail_in;
+		inflateEnd(&strm);
 	        return ss.str();
 	}
 };
